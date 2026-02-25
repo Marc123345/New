62330cc284f8b1394b63f96295684cf6640f2455
Marc123345 <84985026+Marc123345@users.noreply.github.com>
2026-02-25 12:25:19 +0200

    Updated AboutStory.tsx

--- src/components/AboutStory.tsx
+++ src/components/AboutStory.tsx
@@ -232,7 +232,7 @@ export function AboutStory() {
                 From Brand Voice
               </motion.span>
               <motion.span
-                className="block mt-1"
+                className="block mt-1 pb-4"
                 initial={{ opacity: 0, y: 30 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true, amount: 0.2 }}

31aad4b6163a08be9d8613c8fbd3a2d47b84f74c
Marc123345 <84985026+Marc123345@users.noreply.github.com>
2026-03-01 22:03:18 +0200

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -75,14 +75,13 @@ function AppContent() {
 
       <StorySection />
 
-      <div id="ecosystem" className="relative" style={{ zIndex: 2 }}>
-        {/* FIX: Added Suspense wrapper for lazy-loaded EcosystemServices */}
+      <div id="ecosystem" className="relative bg-[var(--color-background-light)]" style={{ zIndex: 2 }}>
         <Suspense fallback={<SectionLoader />}>
           <EcosystemServices />
         </Suspense>
       </div>
 
-      <div id="about" className="relative" style={{ zIndex: 2 }}>
+      <div id="about" className="relative bg-[var(--color-background-light)]" style={{ zIndex: 2 }}>
         <Suspense fallback={<SectionLoader />}>
           <AboutStory />
         </Suspense>
@@ -98,7 +97,7 @@ function AppContent() {
         <Testimonials />
       </Section>
 
-      <div id="blog">
+      <div id="blog" className="bg-[var(--color-background-light)]">
         <LazySection>
           <Suspense fallback={<SectionLoader />}>
             <BlogSection />

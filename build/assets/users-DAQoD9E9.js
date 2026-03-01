9c2bb4134e02aada4be696dd7fa8137e3d31706c
Marc123345 <84985026+Marc123345@users.noreply.github.com>
2026-03-01 19:37:00 +0200

    Publish application

--- build/assets/users-DAQoD9E9.js
+++ build/assets/users-DAQoD9E9.js
@@ -1,4 +1,4 @@
-import{d as e}from"./index-7oWrSRbL.js";/**
+import{d as e}from"./index-24cfl_4I.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
